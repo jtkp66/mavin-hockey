4791203faa757771d61d0ee2ed223f65f6deeec8
Jamie Pham <jtkp66@gmail.com>
2019-08-01 13:13:36 -0700

    updated menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,32 +14,37 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl:
             "https://images.sidelineswap.com/production/003/534/704/dca050c89fd76f7b_small.jpeg",
-          id: 1
+          id: 1,
+          linkUrl: "hats"
         },
         {
           title: "jackets",
           imageUrl:
             "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d384e965528065.5af772a0b2b68.png",
-          id: 2
+          id: 2,
+          linkUrl: ""
         },
         {
           title: "sneakers",
           imageUrl:
             "https://s3.amazonaws.com/rsportz-production/file_assets/attachments/000/000/607/original/adult-hockey-james-sandkuhl.jpg",
-          id: 3
+          id: 3,
+          linkUrl: ""
         },
         {
           title: "womens",
           imageUrl:
             "https://scontent-lga3-1.cdninstagram.com/vp/8db877c40fa3f2fd511443a2e4cb281c/5DCB10C2/t51.2885-15/sh0.08/e35/c162.0.720.720/s640x640/58410453_368931773832845_8231276497642590658_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com",
           size: "large",
-          id: 4
+          id: 4,
+          linkUrl: ""
         },
         {
           title: "mens",
           imageUrl: "https://pbs.twimg.com/media/DhHioFUVAAA18CD.jpg",
           size: "large",
-          id: 5
+          id: 5,
+          linkUrl: ""
         }
       ]
     };
@@ -48,8 +53,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
