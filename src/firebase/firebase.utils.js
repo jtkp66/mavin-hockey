65aaa8a7a4d8f42b11c5f360feedd83845633620
Jamie Pham <jtkp66@gmail.com>
2019-08-01 16:06:53 -0700

    integrated firebase into application, created sign in component and sign in w/ google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,49 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyDW6ldTQ9jIItRGdTDdhroBjBH1TWHaN8o",
+  authDomain: "mavindb-4e0a3.firebaseapp.com",
+  databaseURL: "https://mavindb-4e0a3.firebaseio.com",
+  projectId: "mavindb-4e0a3",
+  storageBucket: "",
+  messagingSenderId: "1053279623099",
+  appId: "1:1053279623099:web:1d401d35d3edfb54"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
