dda15c36d6906ea563005d41fc7f6fdfafd2b001
Jamie Pham <jtkp66@gmail.com>
2019-08-01 21:34:08 -0700

    added carticon annd dropdown, implemented cart reducer, and added memoized selectors

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,6 +3,7 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
 const persistConfig = {
   key: "root",
@@ -11,7 +12,8 @@ const persistConfig = {
 };
 
 const rootReducer = combineReducers({
-  user: userReducer
+  user: userReducer,
+  cart: cartReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
